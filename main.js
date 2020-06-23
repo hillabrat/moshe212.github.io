7192dd5d0bede0e66845e5094412fa2253d35aa7
Moshe <moshe212@gmail.com>
2020-06-23 16:02:57 +0300

    set 2 img for screen

--- main.js
+++ main.js
@@ -3,16 +3,17 @@ var slider = new KeenSlider("#my-keen-slider", {
   slidesPerView: 2,
   centered: true,
   loop: true,
-  mode: "snap",
-  dragSpeed: 10,
-  breakpoints: {
-    "(min-width: 768px)": {
-      slidesPerView: 2,
-      mode: "free-snap",
-    },
-    "(min-width: 1200px)": {
-      slidesPerView: 3,
-      mode: "free-snap",
-    },
-  },
+  centered: false,
+  mode: "free-snap",
+  dragSpeed: 1,
+  //   breakpoints: {
+  //     "(min-width: 768px)": {
+  //       slidesPerView: 2,
+  //       mode: "free-snap",
+  //     },
+  //     "(min-width: 1200px)": {
+  //       slidesPerView: 3,
+  //       mode: "free-snap",
+  //     },
+  //   },
 });
